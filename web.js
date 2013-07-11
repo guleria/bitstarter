6bf40cea46e66fac9a7a8b22ad9712a4911fd15e
Sanjeev K. Guleria <skguleria@thapar.edu>
2013-07-11 18:04:26 +0000

    Updated web.js again

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var buffer = new Buffer(160);
-buffer=fs.readFileSync('index.html');
+buffer=fs.readFileSync('./index.html');
 app.get('/', function(request, response) {
   response.send(buffer.toString("utf-8",0,28));
 });
